7da1043738bba611b27219784ddc99d180d3b9d0
Roger Robertson Jr <rjrjr@pm.me>
2020-03-13 13:50:50 -0700

    Question 1

---
+++ console.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require_relative './review-question-1.rb'
+require_relative './review-question-2.rb'
+require_relative './review-question-3.rb'
+
+#pokemon
+binding.pry
+
+
+0
--- review-question-2.rb
+++ review-question-2.rb
@@ -1,18 +1,31 @@
 # Finish the implementation of the Car class so it has the functionality described below
-
+require 'pry'
 class Car
 
+    @@all = []
+    attr_reader :make, :model
+    def initialize(make: , model: )
+        @make = make
+        @model = model
+        @@all << self
+    end
 
+    def self.all
+        @@all #.each {|car| p "#<" and car.class.name and p car.object_id and ">"}
+    end
 
+    def self.drive
+        "VROOOOOOOOOOOOM!"
+    end
 end
 
 volvo_lightning = Car.new("Volvo", "Lightning")
 yugo = Car.new("Zastava", "Yugo")
 lada = Car.new("AvtoVAZ", "Lada")
 
-volvo_lightning.make
-#=> "Volvo"
-volvo_lightning.model
+# volvo_lightning.make
+# #=> "Volvo"
+# volvo_lightning.model
 #=> "Lightning"
 
 Car.drive
@@ -20,12 +33,13 @@ Car.drive
 
 Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
-
-BONUS:
+#binding.pry
+#BONUS:
 
 volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
 volvo_lightning.make
 #=> "Volvo"
+binding.pry
 volvo_lightning.model
 #=> "Lightning"
